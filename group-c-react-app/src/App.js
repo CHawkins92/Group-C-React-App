e0ce241f33919592c0ab0f577338cfaf451fc359
alexoneill96 <alexander-oneill@hotmail.com>
2021-09-29 15:01:43 +0100

    backup

--- group-c-react-app/src/App.js
+++ group-c-react-app/src/App.js
@@ -1,7 +1,31 @@
 import "./App.css";
+import { BrowserRouter, Route } from "react-router-dom";
+import Create from "./components/Create/Create.js";
+import Delete from "./components/Delete/Delete.js";
+import Read from "./components/Read/Read.js";
+import Update from "./components/Update/Update.js";
+import "semantic-ui-css/semantic.min.css";
+import React from "react";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <div>
+          <h3>React CRUD</h3>
+        </div>
+        <div>
+          <Route exact path="/" component={Create} />
+        </div>
+        <div style={{ marginTop: 25 }}>
+          <Route exact path="/read" component={Read} />
+        </div>
+        <div>
+          <Route path="/update" component={Update} />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
--- group-c-react-app/src/components/Read/Read.js
+++ group-c-react-app/src/components/Read/Read.js
@@ -1,15 +1,86 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import './Read.css';
+import React from "react";
+import "./Read.css";
+import { Table, Button } from "semantic-ui-react";
+import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const Read = () => (
-  <div className="Read">
-    Read Component
-  </div>
-);
+function Read() {
+  const [tableData, setTableData] = useState([]);
+  useEffect(() => {
+    callMockAPIWithAxiosGET();
+  }, []);
 
-Read.propTypes = {};
+  function setPersonData(data) {
+    console.log(data.id);
+    localStorage.setItem("id", data.id);
+    localStorage.setItem("firstName", data.firstName);
+    localStorage.setItem("lastName", data.lastName);
+  }
 
-Read.defaultProps = {};
+  function onDelete(id) {
+    const endpointURL = `https://6151d18b4a5f22001701d46b.mockapi.io/api/v1/people/${id}`;
+    axios
+      .delete(endpointURL)
+      .then(() => callMockAPIWithAxiosGET())
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  function callMockAPIWithAxiosGET() {
+    const endpointURL =
+      "https://6151d18b4a5f22001701d46b.mockapi.io/api/v1/people";
+    axios
+      .get(endpointURL)
+      .then((response) => setTableData(response.data))
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  return (
+    <div className="Read">
+      <Table celled>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>ID</Table.HeaderCell>
+            <Table.HeaderCell>First Name</Table.HeaderCell>
+            <Table.HeaderCell>Last Name</Table.HeaderCell>
+            <Table.HeaderCell>Update</Table.HeaderCell>
+            <Table.HeaderCell>Delete</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+
+        <Table.Body>
+          {tableData.map((data) => {
+            return (
+              <Table.Row>
+                <Table.Cell>{data.id}</Table.Cell>
+                <Table.Cell>{data.firstName}</Table.Cell>
+                <Table.Cell>{data.lastName}</Table.Cell>
+
+                <Table.Cell>
+                  <Link to="/update">
+                    <Button color="green" onClick={() => setPersonData(data)}>
+                      Update
+                    </Button>
+                  </Link>
+                </Table.Cell>
+                <Table.Cell>
+                  <Link to="/read">
+                    <Button color="red" onClick={() => onDelete(data.id)}>
+                      Delete
+                    </Button>
+                  </Link>
+                </Table.Cell>
+              </Table.Row>
+            );
+          })}
+        </Table.Body>
+      </Table>
+    </div>
+  );
+}
 
 export default Read;
